cfad1c4f4117905939db8119de86b9cfad2386b9
Josemaria Quijada <dyoriente0003@upaca.local>
2022-07-27 11:20:06 -0400

    Agregada comparacion de fechas

--- scrap.py
+++ scrap.py
@@ -1,9 +1,16 @@
+from copyreg import constructor
+from xml.etree.ElementTree import tostring
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
+
+
 
 def getExchangeRates():
   URL = "http://www.bcv.org.ve"
   r = requests.get(URL)
+  today = datetime.today().strftime("%Y-%m-%d") 
+    
   soup = BeautifulSoup(r.content, 'html5lib')
 
   euro = soup.find(id="euro").strong.getText().strip()
@@ -11,7 +18,14 @@ def getExchangeRates():
   date = soup.find(id="dolar").parent.find(property="dc:date").attrs['content']
 
   response = {"date": date, "dolar": dolar, "euro": euro}
-  return response
+
+  rateDate = response["date"][0:10]
+
+  if today == rateDate:
+    return response
+
+  return "Tasa no corresponde al dia de hoy"
+
 
 
 print(getExchangeRates())
